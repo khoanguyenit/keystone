aeb029ae77dda072dea69fa977e477f3ee9203f2
Khoa Nguyen <khoanguyen.it@hotmail.com>
2018-09-17 12:34:50 +0700

    update api update

--- admin/server/api/list/update.js
+++ admin/server/api/list/update.js
@@ -11,26 +11,10 @@ module.exports = function (req, res) {
 	}
 	// var updateCount = 0;
 	async.map(req.body.items, function (data, done) {
-		req.list.model.findById(data.id, function (err, item) {
+		req.list.model.findOne({$or: [{_id: data.id}, {keyID: data.id}]}, function (err, item) {
 			if (err) return done({ statusCode: 500, error: 'database error', detail: err, id: data.id });
 			// custom keystones find by id if not found find by keyID, it still not found return 404
-			if (!item) {
-				req.list.model.findOne({ keyID: data.id }, function (err, item) {
-					if (err) return done({ statusCode: 500, error: 'database error', detail: err, id: data.id });
-					// custom keystones find by id if not found find by keyID, it still not found return 404
-					if (!item) return done({ statusCode: 404, error: 'not found', id: data.id });
-					req.list.updateItem(item, data, { files: req.files, user: req.user }, function (err) {
-						if (err) {
-							err.id = data.id;
-							// validation errors send http 400; everything else sends http 500
-							err.statusCode = err.error === 'validation errors' ? 400 : 500;
-							return done(err);
-						}
-						// updateCount++;
-						done(null, req.query.returnData ? req.list.getData(item) : item.id);
-					});
-				});
-			}
+			if (!item) return done({ statusCode: 404, error: 'not found', id: data.id });
 			// end custom keystonejs khoanguyenit
 			req.list.updateItem(item, data, { files: req.files, user: req.user }, function (err) {
 				if (err) {
