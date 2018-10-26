d20809bc5bf230e34f7cb4ba397de061447fb588
Khoa Nguyen <khoanguyen.it@hotmail.com>
2018-10-26 14:57:51 +0700

    update item update function

--- admin/server/api/item/update.js
+++ admin/server/api/item/update.js
@@ -3,7 +3,7 @@ module.exports = function (req, res) {
 	if (!keystone.security.csrf.validate(req)) {
 		return res.apiError(403, 'invalid csrf');
 	}
-	req.list.model.findOne({$or: [{_id: req.params.id}, {keyID: req.params.id}]}, function (err, updatedItem) {
+	req.list.model.findOne({$or: [{_id: req.params.id}, {keyID: req.params.id}]}, function (err, item) {
 		if (err) return res.status(500).json({ error: 'database error', detail: err });
 		if (!item) return res.status(404).json({ error: 'not found', id: req.params.id });
 		req.list.updateItem(item, req.body, { files: req.files, user: req.user }, function (err) {
@@ -14,7 +14,7 @@ module.exports = function (req, res) {
 			}
 			// Reload the item from the database to prevent save hooks or other
 			// application specific logic from messing with the values in the item
-			req.list.model.findById(req.params.id, function (err, updatedItem) {
+			req.list.model.findOne({$or: [{_id: req.params.id}, {keyID: req.params.id}]}, function (err, updatedItem) {
 				res.json(req.list.getData(updatedItem));
 			});
 		});
